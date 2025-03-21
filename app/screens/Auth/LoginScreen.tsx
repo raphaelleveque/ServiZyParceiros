5df31ae14484e02c7b8d94e701f36838b9d30e6a
Raphael Leveque <raphaellev1102@gmail.com>
2025-03-21 19:52:20 -0300

    Adicionando botões de login com Google e Facebook na tela de login

--- app/screens/Auth/LoginScreen.tsx
+++ app/screens/Auth/LoginScreen.tsx
@@ -7,7 +7,10 @@ import {
   SafeAreaView,
   Image,
   StatusBar,
+  TouchableOpacity,
 } from 'react-native';
+import GoogleIcon from '@/assets/images/google.svg';
+import FacebookIcon from '@/assets/images/facebook2.svg';
 
 type LoginScreenProps = StackScreenProps<AuthStackParamList, 'Login'>;
 
@@ -34,6 +37,20 @@ export default function LoginScreen({ navigation }: LoginScreenProps) {
           <Text className="text-secondary mt-4 font-servy">
             Enter your information below
           </Text>
+        </View>
+
+        <View className="flex-row gap-4 mt-6">
+          {/* Botão Google */}
+          <TouchableOpacity className="flex-1 flex-row items-center justify-center bg-white border border-gray-200 rounded-xl py-4">
+            <GoogleIcon width={20} height={20} />
+            <Text className="ml-2 font-urbanist">Google</Text>
+          </TouchableOpacity>
+
+          {/* Botão Facebook */}
+          <TouchableOpacity className="flex-1 flex-row items-center justify-center bg-white border border-gray-200 rounded-xl py-4">
+            <FacebookIcon width={24} height={24} />
+            <Text className="ml-2 font-urbanist">Facebook</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
