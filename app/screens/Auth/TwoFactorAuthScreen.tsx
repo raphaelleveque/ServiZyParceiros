af698c3a804988312e9bcfdb0af9d672cfeb5035
Raphael Leveque <raphaellev1102@gmail.com>
2025-03-24 22:23:12 -0300

    Implement backspace handling in TwoFactorAuthScreen for improved user experience
    - Added functionality to handle backspace key press in the OTP input fields, allowing users to navigate to the previous input when the current one is empty.
    - Wrapped the main content in a TouchableWithoutFeedback to dismiss the keyboard when tapping outside the input fields.

--- app/screens/Auth/TwoFactorAuthScreen.tsx
+++ app/screens/Auth/TwoFactorAuthScreen.tsx
@@ -6,6 +6,7 @@ import {
   StatusBar,
   TextInput,
   Keyboard,
+  TouchableWithoutFeedback,
 } from 'react-native';
 import React, { useState, useRef } from 'react';
 import { AuthStackParamList } from '@/app/types/navigation';
@@ -41,73 +42,95 @@ const TwoFactorAuthScreen = ({ navigation }: TwoFactorAuthScreenProps) => {
     }
   };
 
-  return (
-    <SafeAreaView className="flex-1 bg-white">
-      <StatusBar barStyle="dark-content" backgroundColor="white" translucent />
+  const handleKeyPress = (event: any, index: number) => {
+    // Check if the key pressed is backspace and the current input is empty
+    if (event.nativeEvent.key === 'Backspace' && digits[index] === '') {
+      // Move focus to the previous input
+      if (index > 0) {
+        inputRefs[index - 1].current?.focus();
 
-      {/* Container principal */}
-      <View className="flex-1 px-6">
-        {/* Ícone de voltar */}
-        <View className="mt-10">
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <AntDesign name="left" size={24} color="black" />
+        // Remove the last character from the previous input
+        const newDigits = [...digits];
+        newDigits[index - 1] = '';
+        setDigits(newDigits);
+      }
+    }
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <SafeAreaView className="flex-1 bg-white">
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor="white"
+          translucent
+        />
+
+        {/* Container principal */}
+        <View className="flex-1 px-6">
+          {/* Ícone de voltar */}
+          <View className="mt-10">
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <AntDesign name="left" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+
+          {/* Área do Título */}
+          <View className="mt-6 self-start">
+            <Text className="text-3xl font-urbanist font-bold">
+              Enter OTP Code
+            </Text>
+            <Text className="text-secondary mt-4 font-syne">
+              OTP code has been sent to (11) 99999-9999
+            </Text>
+          </View>
+
+          {/* Área de input */}
+          <View className="mt-8">
+            <View className="flex-row items-center justify-between">
+              {/* Crie 4 caixinhas de input */}
+              {Array.from({ length: 4 }).map((_, index) => (
+                <View
+                  key={index}
+                  className="border border-subtle-border rounded-xl p-3 flex-1 mx-1"
+                >
+                  <TextInput
+                    ref={inputRefs[index]}
+                    className="font-syne text-body py-2 text-center text-4xl"
+                    maxLength={1}
+                    keyboardType="numeric"
+                    value={digits[index]}
+                    onChangeText={(text) => handleChange(text, index)}
+                    onKeyPress={(event) => handleKeyPress(event, index)}
+                  />
+                </View>
+              ))}
+            </View>
+          </View>
+
+          {/* Área de texto */}
+          <View className="mt-2">
+            <Text className="text-secondary font-syne text-right">
+              Resend code in{' '}
+              <Text className="text-primary font-syne">00:59</Text>
+            </Text>
+          </View>
+
+          {/* Área de imagem */}
+          <View className="mt-8 self-center">
+            <TwoFactorAuthImage />
+          </View>
+
+          {/* Botão de enviar */}
+          <TouchableOpacity
+            className="bg-primary rounded-xl py-5 mt-8"
+            onPress={() => navigation.navigate('Main')}
+          >
+            <Text className="text-white font-syne text-center">Verify</Text>
           </TouchableOpacity>
         </View>
-
-        {/* Área do Título */}
-        <View className="mt-6 self-start">
-          <Text className="text-3xl font-urbanist font-bold">
-            Enter OTP Code
-          </Text>
-          <Text className="text-secondary mt-4 font-syne">
-            OTP code has been sent to (11) 99999-9999
-          </Text>
-        </View>
-
-        {/* Área de input */}
-        <View className="mt-8">
-          <View className="flex-row items-center justify-between">
-            {/* Crie 4 caixinhas de input */}
-            {Array.from({ length: 4 }).map((_, index) => (
-              <View
-                key={index}
-                className="border border-subtle-border rounded-xl p-3 flex-1 mx-1"
-              >
-                <TextInput
-                  ref={inputRefs[index]}
-                  className="font-syne text-body py-2 text-center text-4xl"
-                  maxLength={1}
-                  keyboardType="numeric"
-                  value={digits[index]}
-                  onChangeText={(text) => handleChange(text, index)}
-                />
-              </View>
-            ))}
-          </View>
-        </View>
-
-        {/* Área de texto */}
-        <View className="mt-2">
-          <Text className="text-secondary font-syne text-right">
-            Resend code in{' '}
-            <Text className="text-primary font-syne">00:59</Text>
-          </Text>
-        </View>
-
-        {/* Área de imagem */}
-        <View className="mt-8 self-center">
-          <TwoFactorAuthImage />
-        </View>
-
-        {/* Botão de enviar */}
-        <TouchableOpacity
-          className="bg-primary rounded-xl py-5 mt-8"
-          onPress={() => navigation.navigate('Main')}
-        >
-          <Text className="text-white font-syne text-center">Verify</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
 
